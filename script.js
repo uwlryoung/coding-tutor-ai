3e17825f819310a5290a55b554ac980ee3399da8
Lucas Young <48228936+uwlryoung@users.noreply.github.com>
2023-09-17 13:00:27 -0400

    Rearranged some of the consts within the promtFunc. Changed the parser to parse 'promptInput' instead of 'input

--- script.js
+++ script.js
@@ -12,65 +12,34 @@ const model = new OpenAI({
   model: 'gpt-3.5-turbo'
 });
 
-const parser = StructuredOutputParser.fromNamesAndDescriptions({
-  code: "Javascript code that answers the user's question",
-  explanation: "detailed explanation of the example code provided",
-});
-
-const formatInstructions = parser.getFormatInstructions();
-
-
-// console.log({model});
-
 const promptFunc = async (input) => {
   try {
+
+    const parser = StructuredOutputParser.fromNamesAndDescriptions({
+      code: "Javascript code that answers the user's question",
+      explanation: "detailed explanation of the example code provided",
+    });
+
+    const formatInstructions = parser.getFormatInstructions();
+
     const prompt = new PromptTemplate({
-      template: "You are a javascript expert and will answer the user's coding questions thoroughly as possible.\n{format_instructions}\n{question}",
+      template: "You are a javascript expert and will answer the user’s coding questions thoroughly as possible.\n{format_instructions}\n{question}",
       inputVariables: ["question"],
       partialVariables: { format_instructions: formatInstructions }
     });
 
-    const res = await model.call(input);
-
-    if (!res || typeof res !== 'object') {
-      console.error("Invalid API response:", res);
-      return;
-    }
-
     const promptInput = await prompt.format({
       question: input
     });
+
+    const res = await model.call(promptInput);
     
-    // console.log(res);
     console.log(await parser.parse(res));
   }
   catch (err) {
     console.error("Error in promptFunc:", err);
   }
 };
-
-// const promptFunc = async (input) => {
-//   try {
-//     const prompt = new PromptTemplate({
-//       template: "You are a javascript expert and will answer the user’s coding questions thoroughly as possible.\n{format_instructions}\n{question}",
-//       inputVariables: ["question"],
-//       partialVariables: { format_instructions: formatInstructions }
-//     });
-
-//     const res = await model.call(input);
-
-//     const promptInput = await prompt.format({
-//       question: input
-//     });
-    
-//     // console.log(res);
-//     console.log(await parser.parse(res));
-//   }
-//   catch (err) {
-//     console.error("Error in promptFunc:", err);
-//   }
-
-// };
 
 const init = () => {
   inquirer.prompt([
@@ -83,7 +52,6 @@ const init = () => {
     promptFunc(inquirerResponse.name)
   });
 };
-
 
 init();
 
